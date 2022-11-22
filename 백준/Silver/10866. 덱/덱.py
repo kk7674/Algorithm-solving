7b488d5c62595849aa9fb5e9da02265ad53c6451
TAEUKKIM <71546691+kk7674@users.noreply.github.com>
2022-11-23 02:24:00 +0900

    [Silver IV] Title: 덱, Time: 100 ms, Memory: 32440 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+N =int(sys.stdin.readline())
+queue = deque([])
+
+for _ in range(N):
+    ord = list(sys.stdin.readline().split())
+    if len(ord) == 2:
+        if ord[0] == 'push_front':
+            queue.appendleft(int(ord[1])) #좌측(앞)에 삽입
+        elif ord[0] == 'push_back':
+            queue.append(int(ord[1])) #우측(뒤)에 삽입
+    else: # 길이가 1일 경우
+        if ord[0] == 'pop_front':
+            if len(queue) == 0:
+                print(-1)
+            else:
+                print(queue.popleft())
+        elif ord[0] == 'pop_back':
+            if len(queue) == 0:
+                print(-1)
+            else:
+                print(queue.pop())
+        elif ord[0] == 'size':
+            print(len(queue))
+        elif ord[0] == 'empty':
+            if len(queue) == 0:
+                print(1)
+            else:
+                print(0)
+        elif ord[0] == 'front':
+            if len(queue) !=0:
+                print(queue[0])
+            else:
+                print(-1) 
+        elif ord[0] == 'back':
+            if len(queue) !=0:
+                print(queue[-1])
+            else:
+                print(-1)
+        ord.clear()
+            
