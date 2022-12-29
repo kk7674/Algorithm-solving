6838c08e46e2ef90f3b70f3d4b460668479cbde4
TAEUKKIM <71546691+kk7674@users.noreply.github.com>
2022-12-29 22:21:59 +0900

    [Silver I] Title: 포도주 시식, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+num =[]
+dp = [0] * n
+for _ in range(n):
+    num.append(int(input().rstrip()))
+if n <= 2:
+    print(sum(num))
+    exit(0)
+else:    
+    dp[0] = num[0]
+    dp[1] = num[0] + num[1]
+    dp[2] = max(num[1] + num[2], num[0] + num[2], dp[1])    
+
+    for i in range(3, n):
+            dp[i] = max(dp[i-2] + num[i], dp[i-3] + num[i-1] + num[i], dp[i-1])
+print(max(dp))
