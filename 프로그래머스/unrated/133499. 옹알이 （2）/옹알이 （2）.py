0674ed90f85657b3969769d4c09ff8441e357f3d
TAEUKKIM <71546691+kk7674@users.noreply.github.com>
2023-01-08 15:49:11 +0900

    [unrated] Title: 옹알이 (2), Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,9 @@
+def solution(babbling):
+    answer = 0
+    for item in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+            if w*2 not in item:
+                item = item.replace(w,' ')          
+        if len(item.strip()) == 0:
+                answer+=1    
+    return answer
