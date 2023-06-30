f289e0d6061086a5e49db345313f9a03579a0a6e
TAEUKKIM <71546691+kk7674@users.noreply.github.com>
2023-06-30 21:33:23 +0900

    [Silver II] Title: 부분수열의 합, Time: 264 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,24 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-M,N = map(int,input().split()) #총 카드 수, 합 결과
+m,n = map(int,input().split()) #정수 개수, 원하는 합
 arr = list(map(int,input().split()))
-
-def dfs(depth,sum_T):
-    global ans
-
-    if depth == M:
-        return
-    
-    sum_T += arr[depth]
-
-    if sum_T == N:
-        ans+=1
-    
-    dfs(depth + 1, sum_T - arr[depth])
-    dfs(depth + 1, sum_T)
-
-
 ans = 0
-dfs(0,0)
+def recur(depth,T_sum):
+    global ans
+    if depth ==  m:
+        return
+    val = arr[depth]
+    T_sum += val
+    if T_sum == n:
+        ans+=1
+    recur(depth+1,T_sum)
+    recur(depth + 1, T_sum-val)
+
+recur(0,0)
 print(ans)
