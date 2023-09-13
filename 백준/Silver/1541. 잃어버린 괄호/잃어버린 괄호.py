cd924bf2e5b5411663d8c0d34f819822fc00c9c4
TAEUKKIM <71546691+kk7674@users.noreply.github.com>
2023-09-13 13:58:14 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,23 @@
+"""
+부등호 역할이 사실상 -를 무효화 할 수 있다는 점.
+
+30 - 100 + 55 - 50 + 40
+
+"""
+import sys
+start = 0
+num = []
+Eq = input().split('-')
+
+for item in Eq:
+        temp = list(map(int,item.split('+')))
+        num.append(sum(temp))
+
+if len(Eq) >= 2:    
+    start = num[0]
+    num.pop(0)
+    for i in num:
+        start -= i
+    print(start)
+else:
+     print(sum(num))
